1c1835dc566f7d021096b70ab993db7d646da1e8
thanusha-stack <thanusha13062006@gmail.com>
2026-01-28 23:40:24 +0530

    .gitignore added

--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/tripBookingDb");
-  console.log("MongoDB Connected");
-};
-
-module.exports = connectDB;
